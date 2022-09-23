06ad45d5c22d4ce21eee0cdc566a253f7db9bd79
LeandroSimo <leo.bt.12@hotmail.com>
2022-09-22 21:26:53 -0300

    Viewing Task List

--- src/components/taskList/TaskList.tsx
+++ src/components/taskList/TaskList.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 
-type Props = {};
+// CSS
+import styles from "./TaskList.module.css";
 
-const TaskList = (props: Props) => {
-  return <div>Lista de Tarefa</div>;
+// interface
+import { ITask } from "../../interfaces/Task";
+
+type Props = {
+  taskList: ITask[];
+};
+
+const TaskList = ({ taskList }: Props) => {
+  return (
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id}>
+            <p>{task.title}</p>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas</p>
+      )}
+    </>
+  );
 };
 
 export default TaskList;
